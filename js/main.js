c1b8bfcba808cf74385efd20d806b567c1a332d6
Nehemie Zikama <Nehemiezikama@gmail.com>
2018-07-19 12:58:01 +0300

    Putting all the things together, ready for responsive

--- js/main.js
+++ js/main.js
@@ -2,8 +2,24 @@ let expando,relative,click;
       click = "click";
       expando  = document.querySelector(".menubar");
       relative = document.querySelector(".screen_change");
+      body = document.querySelector(`body`);
 	  
 		  expando.addEventListener(click,()=>{
 			 relative.classList.toggle("screen_change");			  
-			 relative.classList.toggle("navf");			  
 });
+/* 
+  expando.addEventListener("blur",(e)=>{
+	
+	  body.addEventListener("mouseover",()=>{
+		  if(25==25){relative.classList.add("screen_change");}
+		  relative.addEventListener(click,()=>{
+		  relative.classList.remove("screen_change");		  
+	  });
+	  
+	  });
+	
+	//else{
+	//  relative.classList.add("screen_change");	
+	  //}		  
+});
+ */
